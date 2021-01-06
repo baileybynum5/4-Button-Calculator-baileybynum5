764ab51dce3641efc5fe8d6791025609eac5252e
baileybynum5 <bbynum1551@colliervilledragons.org>
2021-01-06 19:39:41 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+Create a program that behaves as a 4 button calculator. 
+This program should ask the user:
+
+> Do you want to perform another calculation? 
+
+after one calculation is complete.  
+
+If they answer yes, start the program over if they say no, end the program.
+**Every operation should be a function.**
+
+
+The display should look like this when you run the program (but of course the numbers would not have to be 5 and 7):
+
+> What operation do you want to perform?
+> 1. Add
+> 2. Subrtact
+> 3. Multiply
+> 4. Divide
+
+> 2
+
+> What is the first number?
+> 5
+> What is the second number?
+> 7
+> The answer is: 12
---
+++ main.py
@@ -0,0 +1,28 @@
+def add(x, y):
+  answer = x + y
+  return answer
+
+def subtract(x, y):
+  answer = x - y
+  return answer
+
+def multiply(x, y):
+  answer = x * y
+  return answer
+
+def divide(x, y):
+  answer = x / y
+  return answer
+
+# input defaults to a string
+operation = int(input("What operation do you want to perform?\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n"))
+
+if operation == 1:
+  print("You picked one.")
+elif operation == 2:
+  print("You picked two.")
+
+#print(f"What operation do you want to perform?\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide")
+
+# in order for your code to repeat you have to have a loop (1. for loops and while loops)
+# i suggest that you use a while loop here
