4816c3172019d84e99d15305787558b89eb0efbe
baileybynum5 <bbynum1551@colliervilledragons.org>
2021-01-07 19:40:18 +0000

    im not done

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ def divide(x, y):
   answer = x / y
   return answer
 
-# input defaults to a string
-operation = int(input("What operation do you want to perform?\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n"))
+# create some initial condition for the "some condition"
 
-if operation == 1:
-  print("You picked one.")
-elif operation == 2:
-  print("You picked two.")
+r = "yes"
 
-#print(f"What operation do you want to perform?\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide")
+while r == "yes":
+  # input defaults to a string
+  operation = int(input("What operation do you want to perform?\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n"))
+
+  if operation == 1:
+    print("You picked 1.")
+  elif operation == 2:
+    print("You picked 2.")
+  elif operation == 3:
+    print("You picked 3.")
+  elif operation == 4:
+    print("You picked 4.")
+
+# you ask a question that changes the value of the condition in row 17 ?
 
 # in order for your code to repeat you have to have a loop (1. for loops and while loops).
 # i suggest that you use a while loop here
